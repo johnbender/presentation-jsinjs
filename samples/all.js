0ee9d3ba08ec0ca40e0a032bc24373863e9520a1
John Bender <john.m.bender@gmail.com>
2011-09-11 20:38:53 -0700

    sample js cleanup

--- samples/all.js
+++ samples/all.js
@@ -1,33 +1,36 @@
 $( "#show-it" ).click(function(e) {
-  $( "#jackie" ).addClass( "opaque-animation" );
+	$( "#jackie" ).addClass( "opacity-transition" );
 });
 
-test( "image slides down on click", function() {
+// bad
+test( "image becomes opaque on click", function() {
 	setTimeout(function(){
 		same( $("#jackie").css("opacity"), "1" );
 		start();
-  }, 2100);
+	}, 2100);
 
 	$( "#show-it" ).click();
 
 	stop();
 });
 
+// good
 test( "image becomes opaque on click", function() {
 	$( "#jackie" ).bind( "webkitTransitionEnd", function(){
 		same( $("#jackie").css("opacity"), "1" );
 		start();
-  });
+	});
 
 	$( "#show-it" ).click();
 
 	stop();
 });
 
-$.onlyFor( "/foos", function() {
+// client side integration
+$.onlyForPathname( "/foos", function() {
 	module( "truthsaying" );
-	
+
 	test( "truthyness", function() {
 		assert( true );
-  });
+	});
 });
